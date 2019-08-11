3cf0d98eb8451639dbea422068f2a3d8a11e9d19
wrmsr <timwilloney@gmail.com>
2019-08-11 13:47:57 -0700

    --

--- attic/blob.js
+++ attic/blob.js
@@ -1,7 +1,9 @@
+// node_modules/.bin/browserify blob.js -o bundle.js
+
 _ = require('lodash')
 function main(argsArray) {
     const _args = argsArray || [1, 1, 1, 2];
     const uniqueArray = _.uniq(_args);
     return uniqueArray;
 }
-main();
+console.log(main());
