9a59d942ef96d2afb864b6905a2c2204b883b8c1
wrmsr <timwilloney@gmail.com>
2019-08-13 10:45:20 -0700

    --

--- tokamak-core/src/main/java/com/wrmsr/tokamak/driver/context/StateCache.java
+++ tokamak-core/src/main/java/com/wrmsr/tokamak/driver/context/StateCache.java
@@ -14,6 +14,38 @@
 
 package com.wrmsr.tokamak.driver.context;
 
+import com.wrmsr.tokamak.api.Id;
+import com.wrmsr.tokamak.api.Row;
+import com.wrmsr.tokamak.driver.state.State;
+import com.wrmsr.tokamak.driver.state.StateStorageContext;
+import com.wrmsr.tokamak.node.Node;
+import com.wrmsr.tokamak.node.StatefulNode;
+import com.wrmsr.tokamak.util.Pair;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
 public interface StateCache
 {
+    boolean contains(State state);
+
+    void invalidate(StatefulNode node, Set<Id> ids);
+
+    boolean isInvalidated(StatefulNode node, Id id);
+
+    State setPhantomAttributes(StatefulNode node, Id id, Optional<Row> row);
+
+    void upgradePhantom(StateStorageContext storageCtx, State state, boolean share);
+
+    Collection<State> getStates();
+
+    Map<Node, Set<Id>> getInvalid();
+
+    Optional<Pair.Immutable<Node, Id>> getNextInvalid();
+
+    Map<Id, State> getIdMap(StatefulNode node);
+
+    void flush(StateStorageContext storageCtx);
 }
