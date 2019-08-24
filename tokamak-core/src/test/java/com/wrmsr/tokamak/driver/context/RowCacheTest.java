62d91160f35d7ac207e994ccce0fb0c642142fc4
wrmsr <timwilloney@gmail.com>
2019-08-23 17:23:36 -0700

    --

--- tokamak-core/src/test/java/com/wrmsr/tokamak/driver/context/RowCacheTest.java
+++ tokamak-core/src/test/java/com/wrmsr/tokamak/driver/context/RowCacheTest.java
@@ -16,11 +16,20 @@ package com.wrmsr.tokamak.driver.context;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import com.wrmsr.tokamak.api.Id;
+import com.wrmsr.tokamak.api.Key;
+import com.wrmsr.tokamak.api.SchemaTable;
+import com.wrmsr.tokamak.driver.DriverRow;
+import com.wrmsr.tokamak.driver.LineagePolicy;
+import com.wrmsr.tokamak.node.ScanNode;
+import com.wrmsr.tokamak.type.Type;
 import junit.framework.TestCase;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Optional;
 
 public class RowCacheTest
         extends TestCase
@@ -35,7 +44,11 @@ public class RowCacheTest
 
         assertEquals("ab", m.get(ImmutableSet.of("a", "b")));
         assertEquals("ab", m.get(ImmutableSet.of("b", "a")));
-        assertEquals("ac", m.get(new HashSet() {{ add("c"); add("a"); }}));
+        assertEquals("ac", m.get(new HashSet()
+        {{
+            add("c");
+            add("a");
+        }}));
 
         Map<ImmutableList<String>, String> m2 = ImmutableMap.of(
                 ImmutableList.of("a", "b"), "ab",
@@ -43,7 +56,37 @@ public class RowCacheTest
         );
 
         assertEquals("ab", m2.get(ImmutableList.of("a", "b")));
-        assertEquals("ab", m2.get(new ArrayList() {{ add("a"); add("b"); }}));
+        assertEquals("ab", m2.get(new ArrayList()
+        {{
+            add("a");
+            add("b");
+        }}));
         assertNull(m2.get(ImmutableList.of("b", "a")));
+    }
+
+    public void testRowCacheImpl()
+            throws Throwable
+    {
+        ScanNode scanNode = new ScanNode(
+                "scan",
+                SchemaTable.of("s", "t"),
+                ImmutableMap.of("id", Type.LONG),
+                ImmutableSet.of("id"),
+                ImmutableSet.of(),
+                ImmutableMap.of(),
+                ImmutableMap.of(),
+                Optional.empty());
+
+        RowCache rc = new RowCacheImpl();
+
+        Optional<Collection<DriverRow>> rows = rc.get(scanNode, Key.of("id", 420L));
+
+        rc.put(scanNode, Key.of("id", 420L), ImmutableList.of(
+                new DriverRow(
+                        scanNode,
+                        LineagePolicy.NOP.build(),
+                        Id.of(420L),
+                        new Object[] {420L})
+        ));
     }
 }
