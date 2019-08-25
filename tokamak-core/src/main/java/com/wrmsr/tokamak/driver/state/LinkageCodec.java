f831b9edcf9e2eba5dcc49a6493377999f6937ef
wrmsr <timwilloney@gmail.com>
2019-08-25 14:15:42 -0700

    --

--- tokamak-core/src/main/java/com/wrmsr/tokamak/driver/state/LinkageCodec.java
+++ tokamak-core/src/main/java/com/wrmsr/tokamak/driver/state/LinkageCodec.java
@@ -13,11 +13,46 @@
  */
 package com.wrmsr.tokamak.driver.state;
 
+import com.wrmsr.tokamak.codec.scalar.ListScalarCodec;
+import com.wrmsr.tokamak.codec.scalar.ScalarCodecs;
+import com.wrmsr.tokamak.codec.scalar.VariableLengthScalarCodec;
+import com.wrmsr.tokamak.node.StatefulNode;
 import com.wrmsr.tokamak.util.codec.Codec;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public final class LinkageCodec
         implements Codec<Linkage, byte[]>
 {
+    private static final ListScalarCodec<byte[]> BYTES_LIST_CODEC =
+            new ListScalarCodec<>(
+                    new VariableLengthScalarCodec<>(
+                            ScalarCodecs.BYTES_SCALAR_CODEC));
+
+    private final StatefulNode node;
+
+    public LinkageCodec(StatefulNode node)
+    {
+        this.node = checkNotNull(node);
+    }
+
+    // public byte[] encode(Linkage.IdLinks data)
+    // {
+    //     List<byte[]> bytesList = data.getIds().stream()
+    //             .map(Id::getValue)
+    //             .collect(toImmutableList());
+    //     return BYTES_LIST_CODEC.encodeBytes(bytesList);
+    // }
+
+    // public Linkage.IdLinks decode(byte[] data)
+    // {
+    //     List<byte[]> bytesList = BYTES_LIST_CODEC.decodeBytes(data);
+    //     return new Linkage.IdLinks(
+    //             bytesList.stream()
+    //                     .map(Id::of)
+    //                     .collect(toImmutableSet()));
+    // }
+
     @Override
     public byte[] encode(Linkage data)
     {
