fad36bcb36108e3239580bf11fc885ba2bf214e5
wrmsr <timwilloney@gmail.com>
2019-09-20 17:30:18 -0700

    --

--- tokamak-dist/src/main/java/com/wrmsr/tokamak/dist/GenDepsPom.java
+++ tokamak-dist/src/main/java/com/wrmsr/tokamak/dist/GenDepsPom.java
@@ -119,7 +119,6 @@ public class GenDepsPom
         tr.setOutputProperty(OutputKeys.INDENT, "yes");
         tr.setOutputProperty(OutputKeys.METHOD, "xml");
         tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-        tr.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "roles.dtd");
         tr.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
 
         File pomDepsFile = new File("../pom-deps.xml");
