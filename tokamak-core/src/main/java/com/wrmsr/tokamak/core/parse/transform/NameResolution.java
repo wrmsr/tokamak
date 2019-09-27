cf081307d24aeef0544eef58efda9f672f6a20eb
wrmsr <timwilloney@gmail.com>
2019-09-27 16:31:49 -0700

    --

--- tokamak-core/src/main/java/com/wrmsr/tokamak/core/parse/transform/NameResolution.java
+++ tokamak-core/src/main/java/com/wrmsr/tokamak/core/parse/transform/NameResolution.java
@@ -13,16 +13,22 @@
  */
 package com.wrmsr.tokamak.core.parse.transform;
 
+import com.google.common.collect.ImmutableList;
 import com.wrmsr.tokamak.core.catalog.Catalog;
 import com.wrmsr.tokamak.core.parse.analysis.ScopeAnalysis;
 import com.wrmsr.tokamak.core.parse.tree.AllSelectItem;
+import com.wrmsr.tokamak.core.parse.tree.QualifiedName;
 import com.wrmsr.tokamak.core.parse.tree.QualifiedNameExpression;
 import com.wrmsr.tokamak.core.parse.tree.TreeNode;
 import com.wrmsr.tokamak.core.parse.tree.visitor.AstRewriter;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.collect.ImmutableList.toImmutableList;
 
 public final class NameResolution
 {
@@ -46,9 +52,49 @@ public final class NameResolution
             public TreeNode visitQualifiedNameExpression(QualifiedNameExpression treeNode, Void context)
             {
                 ScopeAnalysis.SymbolRef sr = checkNotNull(sa.getSymbolRefsByNode().get(treeNode));
-                // List<ScopeAnalysis.Scope> hits = sr.getScope().getChildren().stream()
-                //         .filter(s -> s.getSymbols().stream().anyMatch(s -> s.getName().isPresent() && s.getName().get().equals()))
-                return super.visitQualifiedNameExpression(treeNode, context);
+                List<String> parts = treeNode.getQualifiedName().getParts();
+
+                List<ScopeAnalysis.Symbol> hits;
+
+                if (parts.size() == 1) {
+                    String symbolName = parts.get(0);
+
+                    hits = sr.getScope().getChildren().stream()
+                            .map(ScopeAnalysis.Scope::getSymbols)
+                            .flatMap(Set::stream)
+                            .filter(s -> s.getName().isPresent() && s.getName().get().equals(symbolName))
+                            .collect(toImmutableList());
+                }
+
+                else if (parts.size() == 2) {
+                    // TODO: struct traversal
+                    String scopeName = parts.get(0);
+                    String symbolName = parts.get(1);
+
+                    hits = sr.getScope().getChildren().stream()
+                            .filter(s -> s.getName().isPresent() && s.getName().get().equals(scopeName))
+                            .map(ScopeAnalysis.Scope::getSymbols)
+                            .flatMap(Set::stream)
+                            .filter(s -> s.getName().isPresent() && s.getName().get().equals(symbolName))
+                            .collect(toImmutableList());
+                }
+
+                else {
+                    throw new IllegalStateException(Objects.toString(parts));
+                }
+
+                if (hits.size() > 1) {
+                    throw new IllegalStateException(String.format("Ambiguous reference: %s", parts));
+                }
+                else if (hits.isEmpty()) {
+                    throw new IllegalStateException(String.format("Unresolved reference: %s", parts));
+                }
+
+                ScopeAnalysis.Symbol hit = hits.get(0);
+
+                return new QualifiedNameExpression(
+                        new QualifiedName(
+                                ImmutableList.of(hit.getScope().getName().get(), hit.getName().get())));
             }
         }, null);
     }
