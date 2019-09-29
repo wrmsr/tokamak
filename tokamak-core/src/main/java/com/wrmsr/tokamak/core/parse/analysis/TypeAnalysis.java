ba3a36235d88d93f7b303cdfe1053fb191ddcb25
wrmsr <timwilloney@gmail.com>
2019-09-29 16:26:30 -0700

    --

--- tokamak-core/src/main/java/com/wrmsr/tokamak/core/parse/analysis/TypeAnalysis.java
+++ tokamak-core/src/main/java/com/wrmsr/tokamak/core/parse/analysis/TypeAnalysis.java
@@ -11,7 +11,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.wrmsr.tokamak.core.parse.analysis;
 
 import com.wrmsr.tokamak.core.catalog.Catalog;
