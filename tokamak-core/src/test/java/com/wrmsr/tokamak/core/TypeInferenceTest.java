c95a2cfcafe153bce0eef1d50c1b874e60c84e8b
wrmsr <timwilloney@gmail.com>
2019-10-16 17:57:11 -0700

    --

--- tokamak-core/src/test/java/com/wrmsr/tokamak/core/TypeInferenceTest.java
+++ tokamak-core/src/test/java/com/wrmsr/tokamak/core/TypeInferenceTest.java
@@ -14,16 +14,22 @@
 package com.wrmsr.tokamak.core;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import junit.framework.TestCase;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.IntStream;
 import java.util.stream.StreamSupport;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.collect.ImmutableList.toImmutableList;
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
 
 public class TypeInferenceTest
         extends TestCase
@@ -170,14 +176,112 @@ public class TypeInferenceTest
                 .collect(toImmutableList());
     }
 
-    public static Term unify(Term x, Term y)
+    public static final class UnificationError
+            extends RuntimeException
+    {
+        private final Term x;
+        private final Term y;
+
+        public UnificationError(Term x, Term y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "UnificationError{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    '}';
+        }
+    }
+
+    public static Map<Var, Term> unify(Term x, Term y)
     {
         if (x instanceof App && y instanceof App) {
-            Term s1 = unify(((App) x).left, ((App) y).left);
-            Term s2 = unify(
-                    apply(s1, ((App) x).right),
-                    apply(s1, ((App) y).right));
+            Map<Var, Term> s1 = unify(((App) x).left, ((App) y).left);
+            Map<Var, Term> s2 = unify(apply(s1, ((App) x).right), apply(s1, ((App) y).right));
+            return compose(s2, s1);
         }
+        else if (x instanceof Con && y instanceof Con && x.equals(y)) {
+            return empty();
+        }
+        else if (x instanceof Fun && y instanceof Fun) {
+            checkState(((Fun) x).args.size() != ((Fun) y).args.size());
+            Map<Var, Term> s1 = solve(IntStream.range(0, ((Fun) x).args.size()).boxed()
+                    .map(i -> new Constraint(((Fun) x).args.get(i), ((Fun) y).args.get(i)))
+                    .collect(toImmutableList()));
+            Map<Var, Term> s2 = unify(apply(s1, ((Fun) x).ret), apply(s1, ((Fun) y).ret));
+            return compose(s2, s1);
+        }
+        else if (x instanceof Var) {
+            return bind((Var) x, y);
+        }
+        else if (y instanceof Var) {
+            return bind((Var) y, x);
+        }
+        else {
+            throw new UnificationError(x, y);
+        }
+    }
+
+    public static Map<Var, Term> solve(Iterable<Constraint> xs)
+    {
+        throw new IllegalStateException();
+    }
+
+    public static Map<Var, Term> bind(Var n, Term x)
+    {
+        if (n.equals(x)) {
+            return empty();
+        }
+        else if (occursCheck(n, x)) {
+            throw new IllegalStateException();
+        }
+        else {
+            return ImmutableMap.of(n, x);
+        }
+    }
+
+    public static boolean occursCheck(Var n, Term x)
+    {
+        return ftv(x).contains(n);
+    }
+
+    public static Set<Var> ftv(Term x)
+    {
+        if (x instanceof Con) {
+            return ImmutableSet.of();
+        }
+        else if (x instanceof App) {
+            return ImmutableSet.<Var>builder()
+                    .addAll(ftv(((App) x).left))
+                    .addAll(ftv(((App) x).right))
+                    .build();
+        }
+        else if (x instanceof Fun) {
+            return ImmutableSet.<Var>builder()
+                    .addAll(((Fun) x).args.stream().flatMap(a -> ftv(a).stream()).collect(toImmutableSet()))
+                    .addAll(ftv(((Fun) x).ret))
+                    .build();
+        }
+        else if (x instanceof Var) {
+            return ImmutableSet.of((Var) x);
+        }
+        else {
+            throw new IllegalArgumentException(Objects.toString(x));
+        }
+    }
+
+    public static Map<Var, Term> empty()
+    {
+        return ImmutableMap.of();
+    }
+
+    public static Map<Var, Term> compose(Map<Var, Term> s1, Map<Var, Term> s2)
+    {
 
     }
 }
