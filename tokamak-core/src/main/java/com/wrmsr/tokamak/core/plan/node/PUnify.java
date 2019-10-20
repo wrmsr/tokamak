ae75c44a05ffab20fd4f8b6764f5a198b6749bd1
wrmsr <timwilloney@gmail.com>
2019-10-20 16:29:44 -0700

    --

--- tokamak-core/src/main/java/com/wrmsr/tokamak/core/plan/node/PUnify.java
+++ tokamak-core/src/main/java/com/wrmsr/tokamak/core/plan/node/PUnify.java
@@ -15,12 +15,18 @@ package com.wrmsr.tokamak.core.plan.node;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.ImmutableSet;
+import com.wrmsr.tokamak.core.layout.field.FieldCollection;
+import com.wrmsr.tokamak.core.plan.node.visitor.PNodeVisitor;
+import com.wrmsr.tokamak.core.type.Type;
 
 import javax.annotation.concurrent.Immutable;
 
 import java.util.Set;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
+import static com.wrmsr.tokamak.util.MorePreconditions.checkNotEmpty;
 
 @Immutable
 public final class PUnify
@@ -28,19 +34,62 @@ public final class PUnify
         implements PSingleSource
 {
     private final PNode source;
+    private final Set<String> unifiedFields;
+    private final String outputField;
+
+    private final FieldCollection fields;
 
     @JsonCreator
     public PUnify(
             @JsonProperty("name") String name,
-            PNodeAnnotations annotations,
-            PNode source,
-            Set<String> unifiedFields,
-            String outputField)
+            @JsonProperty("annotations") PNodeAnnotations annotations,
+            @JsonProperty("source") PNode source,
+            @JsonProperty("unifiedFields") Set<String> unifiedFields,
+            @JsonProperty("outputField") String outputField)
     {
         super(name, annotations);
 
         this.source = checkNotNull(source);
+        this.unifiedFields = checkNotEmpty(ImmutableSet.copyOf(unifiedFields));
+        this.outputField = checkNotEmpty(outputField);
+
+        Type unifiedType = source.getFields().getType(checkNotNull(this.unifiedFields.iterator().next()));
+        this.unifiedFields.forEach(uf -> {
+            checkNotEmpty(uf);
+            checkState(source.getFields().getType(uf).equals(unifiedType));
+        });
 
         checkInvariants();
+    }
+
+    @JsonProperty("source")
+    @Override
+    public PNode getSource()
+    {
+        return source;
+    }
+
+    @JsonProperty("unifiedFields")
+    public Set<String> getUnifiedFields()
+    {
+        return unifiedFields;
+    }
+
+    @JsonProperty("outputField")
+    public String getOutputField()
+    {
+        return outputField;
+    }
+
+    @Override
+    public FieldCollection getFields()
+    {
+        return fields;
+    }
+
+    @Override
+    public <R, C> R accept(PNodeVisitor<R, C> visitor, C context)
+    {
+        return visitor.visitUnify(this, context);
     }
 }
