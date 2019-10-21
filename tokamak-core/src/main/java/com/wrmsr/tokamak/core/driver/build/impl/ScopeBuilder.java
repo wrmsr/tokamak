58b2d8d722220afd9eb025e0e953f1896d9a4e29
wrmsr <timwilloney@gmail.com>
2019-10-20 18:06:22 -0700

    --

---
+++ tokamak-core/src/main/java/com/wrmsr/tokamak/core/driver/build/impl/ScopeBuilder.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wrmsr.tokamak.core.driver.build.impl;
+
+import com.wrmsr.tokamak.api.Key;
+import com.wrmsr.tokamak.core.driver.DriverImpl;
+import com.wrmsr.tokamak.core.driver.build.Builder;
+import com.wrmsr.tokamak.core.driver.build.ops.BuildOp;
+import com.wrmsr.tokamak.core.driver.context.DriverContextImpl;
+import com.wrmsr.tokamak.core.plan.node.PNode;
+import com.wrmsr.tokamak.core.plan.node.PScope;
+
+import java.util.Map;
+import java.util.function.Consumer;
+
+public final class ScopeBuilder
+        extends SingleSourceBuilder<PScope>
+{
+    public ScopeBuilder(DriverImpl driver, PScope node, Map<PNode, Builder> sources)
+    {
+        super(driver, node, sources);
+    }
+
+    @Override
+    protected void innerBuild(DriverContextImpl dctx, Key key, Consumer<BuildOp> opConsumer)
+    {
+        throw new IllegalStateException();
+    }
+}
