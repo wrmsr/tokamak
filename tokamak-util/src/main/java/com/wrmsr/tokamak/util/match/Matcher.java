8518d914c88bbdb2184cf8fafce69bd528f9cf85
wrmsr <timwilloney@gmail.com>
2020-02-04 11:54:49 -0800

    --

--- tokamak-util/src/main/java/com/wrmsr/tokamak/util/match/Matcher.java
+++ tokamak-util/src/main/java/com/wrmsr/tokamak/util/match/Matcher.java
@@ -16,15 +16,42 @@ package com.wrmsr.tokamak.util.match;
 import com.wrmsr.tokamak.util.match.pattern.CapturePattern;
 import com.wrmsr.tokamak.util.match.pattern.EqualsPattern;
 import com.wrmsr.tokamak.util.match.pattern.FilterPattern;
+import com.wrmsr.tokamak.util.match.pattern.Pattern;
 import com.wrmsr.tokamak.util.match.pattern.TypeOfPattern;
 import com.wrmsr.tokamak.util.match.pattern.WithPattern;
 import com.wrmsr.tokamak.util.match.pattern.visitor.PatternVisitor;
 
-public class Matcher<M> extends PatternVisitor<Match<M>, Matcher<M>.Context>
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class Matcher<M>
+        extends PatternVisitor<Match<M>, Matcher<M>.Context>
 {
     public final class Context
     {
+        private final Object object;
+        private final Captures captures;
 
+        public Context(Object object, Captures captures)
+        {
+            this.object = checkNotNull(object);
+            this.captures = checkNotNull(captures);
+        }
+    }
+
+    public <T> Match<T> match(Pattern<T> pattern, Object object, Captures captures)
+    {
+        return match(pattern, new Context(object, captures));
+    }
+
+    public <T> Match<T> match(Pattern<T> pattern, Context context)
+    {
+        if (pattern.getNext().isPresent()) {
+            Match<?> match = match(pattern.getNext().get(), context);
+            return match.flatMap((value) -> pattern.accept(this, new Context(value, match.getCaptures())));
+        }
+        else {
+            return pattern.accept(this, context);
+        }
     }
 
     @Override
