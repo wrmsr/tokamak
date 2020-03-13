0606ed43fa2a8f789b6d0ce14b3e096c04015aac
wrmsr <timwilloney@gmail.com>
2020-03-12 20:07:21 -0700

    --

--- tokamak-core/src/test/java/com/wrmsr/tokamak/core/GuiceTest.java
+++ tokamak-core/src/test/java/com/wrmsr/tokamak/core/GuiceTest.java
@@ -13,6 +13,8 @@
  */
 package com.wrmsr.tokamak.core;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
@@ -22,7 +24,11 @@ import com.google.inject.TypeLiteral;
 import com.google.inject.name.Names;
 import junit.framework.TestCase;
 
+import java.util.Map;
+import java.util.Set;
 import java.util.function.Supplier;
+
+import static com.google.inject.multibindings.Multibinder.newSetBinder;
 
 public class GuiceTest
         extends TestCase
@@ -72,5 +78,76 @@ public class GuiceTest
         Injector injector = Guice.createInjector(new IntForwarderModule("a", 1), new IntForwarderModule("b", 2));
         System.out.println(injector.getInstance(Key.get(new TypeLiteral<Supplier<Integer>>() {}, Names.named("a"))).get());
         System.out.println(injector.getInstance(Key.get(new TypeLiteral<Supplier<Integer>>() {}, Names.named("b"))).get());
+    }
+
+    public void testAmbiguousGenerics()
+            throws Throwable
+    {
+        Injector injector = Guice.createInjector(
+                new AbstractModule()
+                {
+                    @Override
+                    protected void configure()
+                    {
+                        bind(new TypeLiteral<Map<String, Integer>>() {}).toInstance(ImmutableMap.of("a", 1));
+                        bind(new TypeLiteral<Map<Integer, String>>() {}).toInstance(ImmutableMap.of(2, "b"));
+                    }
+                });
+
+        System.out.println(injector.getInstance(Key.get(new TypeLiteral<Map<String, Integer>>() {})));
+        System.out.println(injector.getInstance(Key.get(new TypeLiteral<Map<Integer, String>>() {})));
+
+        // System.out.println(injector.getInstance(Key.get(new TypeLiteral<Map<Integer, Object>>() {})));
+    }
+
+    public void testPrivateOverrides()
+            throws Throwable
+    {
+        Injector injectorBase = Guice.createInjector(
+                new AbstractModule()
+                {
+                    @Override
+                    protected void configure()
+                    {
+                        newSetBinder(binder(), Integer.class).addBinding().toInstance(-1);
+                    }
+                });
+
+        System.out.println(injectorBase.getInstance(Key.get(new TypeLiteral<Set<Integer>>() {})));
+
+        Injector injector0 = injectorBase.createChildInjector(
+                new AbstractModule()
+                {
+                    @Override
+                    protected void configure()
+                    {
+                        // bind(Integer.class).toInstance(0);
+                        newSetBinder(binder(), Integer.class).addBinding().toInstance(0);
+                    }
+                });
+
+        System.out.println(injector0.getInstance(Key.get(new TypeLiteral<Set<Integer>>() {})));
+
+        Injector injector1 = injector0.createChildInjector(new PrivateModule()
+        {
+            @Override
+            protected void configure()
+            {
+                newSetBinder(binder(), Integer.class).addBinding().toInstance(1);
+            }
+        });
+
+        System.out.println(injector1.getInstance(Key.get(new TypeLiteral<Set<Integer>>() {})));
+
+        Injector injector2 = injector0.createChildInjector(new PrivateModule()
+        {
+            @Override
+            protected void configure()
+            {
+                newSetBinder(binder(), Integer.class).addBinding().toInstance(2);
+            }
+        });
+
+        System.out.println(injector2.getInstance(Key.get(new TypeLiteral<Set<Integer>>() {})));
     }
 }
